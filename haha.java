15d113b65f88cf0e52b6aa41e23a61ba710bd95d
devesg <a01094832297@gmail.com>
2022-12-26 15:11:29 +0900

    :white_check_mark:

---
+++ haha.java
@@ -0,0 +1,9 @@
+public class haha {
+    public static void main(String[] args) {
+        WhatisSongJeongMin();
+    }
+
+    protected static void WhatisSongJeongMin() {
+        System.out.println("Do you know?");
+    }
+}
